db817b153a852bd2b29eaea9f4610992aa8c71a2
Tom Xia <xia73@wisc.edu>
2025-12-23 04:28:10 -0600

    configs: AtomicSimpleCPU in mi200 model
    added atomic simple cpu as an option
    
    Change-Id: I964dffa90d05f5769df98da36fb51d89752e810e

--- configs/example/gpufs/mi200.py
+++ configs/example/gpufs/mi200.py
@@ -139,8 +139,9 @@ def runMI200GPUFS(cpu_type):
 
     # Defaults for MI200
     args.ruby = True
-    args.cpu_type = "X86KvmCPU"
-    args.mem_size = "8GB"  # CPU host memory
+    if args.cpu_type != "X86KvmCPU" and args.cpu_type != "AtomicSimpleCPU":
+        args.cpu_type = "X86KvmCPU"
+    args.mem_size = "8GiB"  # CPU host memory
     args.dgpu = True
     args.dgpu_mem_size = "16GB"  # GPU device memory
     args.dgpu_start = "0GB"
